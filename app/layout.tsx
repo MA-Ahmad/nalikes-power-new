e51ef161324d78418882276da5e36bb3133906a5
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2025-08-08 18:09:27 +0500

    chore: small update

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
             <WalletProvider>
               <ThemeProvider
                 attribute="class"
-                defaultTheme="system"
+                defaultTheme="dark"
                 enableSystem
                 disableTransitionOnChange
               >
