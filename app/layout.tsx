13888343795bd516b16ed79539c1582b4ac343aa
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2026-01-15 17:39:00 +0500

    feat: add google tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ import {
   aeonikMonoRegular,
   aeonikRegular,
 } from './fonts'
+import Script from 'next/script'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -30,6 +31,9 @@ export const metadata: Metadata = {
   title: 'Power.win - Crypto Casino where the edge belongs to the player',
   description:
     'Play games, win prizes, and climb the leaderboard with Power.win',
+  verification: {
+    google: 'mREN--1L8N-o5TCL03M9AKNu7ntX2Zxl9gqYxCF6jco',
+  },
 }
 
 export default function RootLayout({
@@ -43,6 +47,44 @@ export default function RootLayout({
         // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         className={`${aeonikRegular.variable} ${aeonikBold.variable} ${aeonikLight.variable} ${aeonikMedium.variable} ${aeonikMonoRegular.variable} ${aeonikMonoBlack.variable}`}
       >
+        {/* Google Tag Manager - loads early for proper tracking */}
+        <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-THNLQSJL');`,
+          }}
+        />
+        {/* Google Analytics 4 */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-VX3GYJ2KTF"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="ga4-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-VX3GYJ2KTF');
+            `,
+          }}
+        />
+        {/* Google Tag Manager (noscript) - fallback for users with JavaScript disabled */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-THNLQSJL"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <QueryProvider>
           <AuthProvider>
             <ThemeProvider
